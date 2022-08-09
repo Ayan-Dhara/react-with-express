438c67fb9e58ec8a1e0587f7c635db67ea6441ff
Ayan Dhara <git@ayandhara.me>
2022-08-09 13:38:17 +0530

    updated dependencies and upgraded to react 18

--- template/fullstack/src/index.tsx
+++ template/fullstack/src/index.tsx
@@ -1,11 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./components/App";
+import {createRoot} from "react-dom/client";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+createRoot(document.getElementById("root") as Element).render(<React.StrictMode><App/></React.StrictMode>)
